07df617aae0c0924a2d9db0b49c8ec3f42d7f785
AlbertoJTD <albertojtd@outlook.com>
2023-05-21 14:48:31 -0600

    Add create method

--- BeerApp/Controllers/BeerController.cs
+++ BeerApp/Controllers/BeerController.cs
@@ -1,5 +1,7 @@
 ﻿using BeerApp.Models;
+using BeerApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeerApp.Controllers
@@ -19,6 +21,31 @@ namespace BeerApp.Controllers
 			return View(await beers.ToListAsync());
 		}
 
+		public IActionResult Create()
+		{
+			ViewData["Brands"] = new SelectList(_context.Brands, "BrandId", "Name");
+			return View();
+		}
 
+		[HttpPost]
+		[ValidateAntiForgeryToken] // espera la informacion del formulario del sitio en cuestion
+		public async Task<IActionResult> Create(BeerViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var beer = new Beer()
+				{
+					Name = model.Name,
+					BrandId = model.BrandId,
+				};
+				_context.Add(beer);
+				await _context.SaveChangesAsync();
+
+				return RedirectToAction(nameof(Index)); // Retornar al metodo index
+			}
+
+			ViewData["Brands"] = new SelectList(_context.Brands, "BrandId", "Name", model.BrandId);
+			return View(model);
+		}
 	}
 }
---
+++ BeerApp/Models/ViewModels/BeerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeerApp.Models.ViewModels
+{
+	public class BeerViewModel
+	{
+		[Required]
+        [Display(Name = "Nombre")] // Mostrar 'Nombre' en lugar de 'Name'
+        public string Name { get; set; }
+
+		[Required]
+		[Display(Name = "Marca")]
+		public int BrandId { get; set; }
+    }
+}
