77f05a645031633e3c633e24486f9d0f76eaaab6
AlbertoJTD <albertojtd@outlook.com>
2023-05-21 12:40:47 -0600

    Add Beer section

---
+++ BeerApp/Controllers/BeerController.cs
@@ -0,0 +1,24 @@
+﻿using BeerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerApp.Controllers
+{
+	public class BeerController : Controller
+	{
+		private readonly BeerAppContext _context;
+
+        public BeerController(BeerAppContext context)
+        {
+			_context = context;
+        }
+
+        public async Task<IActionResult> Index()
+		{
+			var beers = _context.Beers.Include(brand => brand.Brand);
+			return View(await beers.ToListAsync());
+		}
+
+
+	}
+}
